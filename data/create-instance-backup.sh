c6a492832352deebe6be3d7978f77b5420cbcb51
David Poetzsch-Heffter <david.poetzsch-heffter@mondata.de>
2020-09-24 15:28:33 +0200

    fixed create instance script

--- data/create-instance-backup.sh
+++ data/create-instance-backup.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
+INSTANCE_NAME=$1
+
 # create new nginx config
 cp testfarm-nginx-conf/conf.d/sites-enabled/testfarm-wp-vanilla.conf \
-  testfarm-nginx-conf/conf.d/sites-enabled/testfarm-$2.conf
-sed -i -e "s/wp-vanilla/$2/g" testfarm-nginx-conf/conf.d/sites-enabled/testfarm-$2.conf
+  testfarm-nginx-conf/conf.d/sites-enabled/testfarm-$INSTANCE_NAME.conf
+sed -i -e "s/wp-vanilla/$INSTANCE_NAME/g" testfarm-nginx-conf/conf.d/sites-enabled/testfarm-$INSTANCE_NAME.conf
 
 # create new empty backup
-mkdir /tmp/testfarm-$2-wwwroot
+mkdir /tmp/testfarm-$INSTANCE_NAME-wwwroot
 
-sudo chown -R 33.33 /tmp/testfarm-$2-wwwroot # www-data
+sudo chown -R 33.33 /tmp/testfarm-$INSTANCE_NAME-wwwroot # www-data
 cd /tmp
-tar czf $OLDPWD/testfarm-$2-wwwroot.tar.gz testfarm-$2-wwwroot
+tar czf $OLDPWD/testfarm-$INSTANCE_NAME-wwwroot.tar.gz testfarm-$INSTANCE_NAME-wwwroot
